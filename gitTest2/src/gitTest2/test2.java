252735a5fc896de43ad861321b01eef4c893517b
Abhiran143 <kiranjape@gmail.com>
2020-07-08 10:28:47 +0530

    Second Commit new test2.java

---
+++ gitTest2/src/gitTest2/test2.java
@@ -0,0 +1,10 @@
+package gitTest2;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println("Hello World this is Eclipse from Kiran to Git ");
+	}
+
+}
